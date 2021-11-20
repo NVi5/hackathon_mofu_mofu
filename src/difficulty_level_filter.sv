c83382c4cf1653e06047365c41eedd51be83bf75
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-11-20 13:56:09 +0100

    Fix condifion in level filter & refactoring

--- src/difficulty_level_filter.sv
+++ src/difficulty_level_filter.sv
@@ -1,23 +1,23 @@
 module difficulty_level_filter
 #(
-    parameter [7:0] difficulty = 8'b0
+  parameter [7:0] difficulty = 8'b0
 )
 (
-    input wire clk,
+  input wire clk,
 
-    input wire [127:0] data_i,
-    input wire         data_i_valid,
+  input wire [127:0] data_i,
+  input wire         valid_i,
 
-    output reg [127:0] data_o,
-    output reg         data_o_valid
+  output reg [127:0] data_o,
+  output reg         valid_o
 );
 
 always_ff @(posedge clk) begin
-    data_o <= data_i;
-    data_o_valid <= 1'b0;
-    if (data_i[7:0] <= difficulty) begin
-        data_o_valid <= 1'b1;
-    end
+  data_o <= data_i;
+  valid_o <= 1'b0;
+  if (valid_i && data_i[7:0] <= difficulty) begin
+    valid_o <= 1'b1;
+  end
 end
 
 endmodule
--- src/validator.sv
+++ src/validator.sv
@@ -22,11 +22,11 @@ difficulty_level_filter #(.difficulty(8'b0)) u_difficulty_level_filter
 (
     .clk(clk),
 
-    .data_i(i_transcation),
-    .data_i_valid(i_valid),
+    .data_i   (i_transcation),
+    .valid_i  (i_valid),
 
-    .data_o(difficulty_filter_o),
-    .data_o_valid(difficulty_filter_valid)
+    .data_o   (difficulty_filter_o),
+    .valid_o  (difficulty_filter_valid)
 );
 
 trans_validator u_trans_validator
@@ -34,11 +34,11 @@ trans_validator u_trans_validator
     .clk(clk),
     .rst(rst),
 
-    .data_i(difficulty_filter_o),
-    .valid_i(difficulty_filter_valid),
+    .data_i   (difficulty_filter_o),
+    .valid_i  (difficulty_filter_valid),
 
-    .data_o(trans_validator_o),
-    .valid_o(trans_validator_valid)
+    .data_o   (trans_validator_o),
+    .valid_o  (trans_validator_valid)
 );
 
 hash_gen u_hash_gen
