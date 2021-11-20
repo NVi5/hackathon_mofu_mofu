d9f21d625fa9bc21144cbab50987e3b75e8c2469
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-11-20 12:38:30 +0100

    Fix

--- src/validator.sv
+++ src/validator.sv
@@ -14,7 +14,11 @@ module validator(
 wire [127:0] difficulty_filter_o;
 wire         difficulty_filter_valid;
 
-difficulty_level_fitler u_difficulty_level_fitler #(.difficulty(8'b0))
+wire [127:0] trans_validator_o;
+wire         trans_validator_valid;
+
+
+difficulty_level_filter #(.difficulty(8'b0)) u_difficulty_level_filter
 (
     .clk(clk),
 
@@ -30,19 +34,19 @@ trans_validator u_trans_validator
     .clk(clk),
     .rst(rst),
 
-    .data_i(i_transcation),
-    .data_i_valid(i_valid),
+    .data_i(difficulty_filter_o),
+    .data_i_valid(difficulty_filter_valid),
 
-    .data_o(difficulty_filter_o),
-    .data_o_valid(difficulty_filter_valid)
+    .data_o(trans_validator_o),
+    .data_o_valid(trans_validator_valid)
 );
 
 hash_gen u_hash_gen
 (
     .clk     (clk),
 
-    .data_i  (difficulty_filter_o),
-    .valid_i (difficulty_filter_valid),
+    .data_i  (trans_validator_o),
+    .valid_i (trans_validator_valid),
 
     .data_o  (o_hash),
     .valid_o (o_valid)
