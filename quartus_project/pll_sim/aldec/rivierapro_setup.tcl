5e33443411a04ef2be9d819034b13aa3616090b7
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-11-21 01:35:24 +0100

    Add pll again

--- quartus_project/pll_sim/aldec/rivierapro_setup.tcl
+++ quartus_project/pll_sim/aldec/rivierapro_setup.tcl
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 20.1 720 win32 2021.11.20.23:27:47
+# ACDS 20.1 720 win32 2021.11.21.01:34:51
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
