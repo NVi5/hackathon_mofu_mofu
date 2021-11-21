1b78263810be1bef24c7d0ca47d64c23e7d3ecac
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-11-21 01:51:55 +0100

    Add pll

--- src/validator.sv
+++ src/validator.sv
@@ -13,7 +13,7 @@ module validator(
 
 wire [127:0] difficulty_filter_o;
 wire         difficulty_filter_valid;
-difficulty_level_filter #(.difficulty(8'b0)) u_difficulty_level_filter
+difficulty_level_filter #(.difficulty(8'b1)) u_difficulty_level_filter
 (
     .clk      (clk),
 
@@ -24,17 +24,26 @@ difficulty_level_filter #(.difficulty(8'b0)) u_difficulty_level_filter
     .valid_o  (difficulty_filter_valid)
 );
 
+wire clk_2;
+pll u_pll
+(
+    .refclk   (clk),
+    .rst      (rst),
+    .outclk_0 (clk_2)
+);
+
 wire [127:0] fifo_o;
 wire         fifo_empty;
-fifo u_fifo
+fifo_dual u_fifo
 (
-    .clock    (clk),
+    .wrclk    (clk),
+    .rdclk    (clk_2),
 
     .data     (difficulty_filter_o),
     .wrreq    (difficulty_filter_valid),
 
     .q        (fifo_o),
-    .empty    (fifo_empty),
+    .rdempty  (fifo_empty),
 
     .rdreq    (trans_validator_ack)
 );
@@ -44,7 +53,7 @@ wire         trans_validator_valid;
 wire         trans_validator_ack;
 trans_validator u_trans_validator
 (
-    .clk      (clk),
+    .clk      (clk_2),
 
     .data_i   (fifo_o),
     .valid_i  (!fifo_empty),
@@ -54,12 +63,29 @@ trans_validator u_trans_validator
     .ack_o    (trans_validator_ack)
 );
 
+
+wire [127:0] fifo_2_o;
+wire         fifo_2_empty;
+fifo_dual u_fifo_2
+(
+    .wrclk    (clk_2),
+    .rdclk    (clk),
+
+    .data     (trans_validator_o),
+    .wrreq    (trans_validator_valid),
+
+    .q        (fifo_2_o),
+    .rdempty  (fifo_2_empty),
+
+    .rdreq    (!fifo_2_empty)
+);
+
 hash_gen u_hash_gen
 (
     .clk      (clk),
 
-    .data_i   (trans_validator_o),
-    .valid_i  (trans_validator_valid),
+    .data_i   (fifo_2_o),
+    .valid_i  (!fifo_2_empty),
 
     .data_o   (o_hash),
     .valid_o  (o_valid),
